8aafec5791f33e6360b15969b51840148a0a7b1f
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2023-10-15 01:26:14 +0500

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,33 +6,33 @@ function Navbar ()
     return (
       <div>
          <header >
-        <nav className='flex items-center justify-between h-14 pl-20 '>
+        <nav className=' font-sans flex items-center justify-between h-14 pl-20 '>
             <div className='flex items-center'>
-                <Image className=' h-5 w-16' src={Logo} />
+                <Image className='  h-5 w-16' src={Logo} />
             </div>
-            <ul className='flex items-center justify-center gap-2'>
-                <li className='grid items-center'>
+            <ul className='flex items-center justify-center gap-6'>
+                <li className='font-semibold grid items-center'>
                     New & Feature
                 </li>
-                <li className='grid items-center'>
+                <li className=' font-semibold grid items-center'>
                     Man
                 </li>
-                <li className='grid items-center'>
+                <li className='grid items-center font-semibold'>
                     Women
                 </li>
-                <li className='grid items-center'>
+                <li className='grid items-center font-semibold'>
                     Kids
                 </li>
-                <li className='grid items-center'>
+                <li className='grid items-center font-semibold'>
                     Accessories
                 </li>
-                <li className='grid items-center'>
+                <li className='grid items-center font-semibold'>
                     Sales
                 </li>
             </ul>
-            <ul className='flex items-center justify-center gap-2'>
+            <ul className='flex items-center justify-center gap-6'>
                 <li className='grid items-center'>
-                    <input className=' bg-[#f5f5f5] border-none' />
+                    <input className=' bg-[#f5f5f5] border-none rounded-2xl h-8' />
                 </li>
                 <li className='grid items-center'>
                
