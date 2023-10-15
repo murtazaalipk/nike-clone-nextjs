2995f23d3122bad273869c6312b333439c3c5ee2
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2023-10-15 13:01:26 +0500

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,6 +4,7 @@ import Head2 from "../assets/head2.png"
 import Image from 'next/image'
 import Bag from "../assets/bag.png"
 import Heart from "../assets/heart.png"
+import Link from "next/link"
 
 export default function Header() {
   return (
@@ -11,25 +12,25 @@ export default function Header() {
         <div className=" bg-[#f5f5f5] h-9 flex justify-between pl-16 pr-16">
         <ul className='flex items-center justify-center gap-6'>
                 <li className='grid items-center'>
-                <Image className="h-8 w-8" src={Head1} />
+                <Link href={"./"} >  <Image className="h-8 w-8" src={Head1} /> </Link>
                 </li>
                 <li className='grid items-center'>
-                <Image className="" src={Head2} />
+                <Link href={"./"} >  <Image className="" src={Head2} /> </Link>
                 </li>
  
             </ul>
             <ul className='flex items-center justify-center gap-4'>
                 <li className='font-semibold grid items-center'>
-                    Find a Store &nbsp; |
+                <Link href={"./"} >   Find a Store &nbsp; | </Link>
                 </li>
                 <li className=' font-semibold grid items-center'>
-                    Help &nbsp; |
+                <Link href={"./"} >    Help &nbsp; | </Link>
                 </li>
                 <li className=' font-semibold grid items-center'>
-                    Join Us &nbsp; |
+                <Link href={"./"} >   Join Us &nbsp; | </Link>
                 </li>
                 <li className=' font-semibold  grid items-center'>
-                    Sign In 
+                <Link href={"./"} >   Sign In  </Link>
                 </li>
  
             </ul>
