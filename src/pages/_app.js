f0956f21eea84780feb36360938076432ade9b0b
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2023-10-21 18:40:35 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
+import Footer from '@/components/footer'
+import Header from '@/components/header'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <body className=' w-full'>
+  <Header/>
+  <Component {...pageProps} />
+  <Footer/>
+  </body>
+
+  )
 }
