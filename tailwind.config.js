594d5191009d2cf4e24d8dc67dce4c0e82c0a59e
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2023-10-21 18:41:21 +0500

    Add Screen Sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      
+      screens: {
+        'sm': { 'max': "600px" },
+        'md': { 'max': "966px" },
+        'lg': { 'max': "991px" },
+        'xl': { 'max': "1200px" },
+        // reverse
+        'mdr': { 'min': "966px" },
+        'smr': { 'min': "600px" },
+      }
     },
   },
   plugins: [],
